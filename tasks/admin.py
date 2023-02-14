c0cac7a7891dd7a5b43c6e10def21366e70ce6c4
Saeid Heidari <saeiiid.heidari@gmail.com>
2023-02-14 19:42:06 +0330

    configure tasks/admin.py.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import Profile, Task
 
-# Register your models here.
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 1
+    fields = (
+        "title",
+        "status",
+        "created",
+        "updated",
+    )
+    readonly_fields = (
+        "created",
+        "updated",
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "status",
+        "created",
+        "updated",
+    )
+    readonly_fields = (
+        "created",
+        "updated",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
+
+    search_fields = ("title",)
+    list_filter = (
+        "status",
+        "created",
+        "updated",
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = (
+        "user",
+        "bio",
+        "birth_date",
+    )
+    list_display = (
+        "user",
+        "bio",
+        "birth_date",
+    )
+    list_filter = ("user",)
+
+    list_select_related = ("user",)
+
+    inlines = (TaskInline,)
