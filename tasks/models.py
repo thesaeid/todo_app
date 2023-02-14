a87a40699e5684b7dda93e22b10f00c64e50de4f
Saeid Heidari <saeiiid.heidari@gmail.com>
2023-02-14 19:41:33 +0330

    update tasks/models.py. models configured

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
+from uuid import uuid4
 from django.db import models
+from django.conf import settings
+from django.template.defaultfilters import truncatechars
 
-# Create your models here.
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ("todo", "To Do"),
+        ("in_progress", "In Progress"),
+        ("done", "Done"),
+    )
+
+    title = models.CharField(max_length=255)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="tasks")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="todo")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return truncatechars(self.title, 15)
