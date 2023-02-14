fc7d51b82e01b21ebf4a2deb942e700ca4e431b9
Saeid Heidari <saeiiid.heidari@gmail.com>
2023-02-14 19:46:31 +0330

    configure tasks/views.py.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Task, Profile
+from .serializers import (
+    TaskSerializer,
+    TaskCreateSerializer,
+    ProfileSerializer,
+    ProfileCreateSerializer,
+)
 
-# Create your views here.
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return TaskCreateSerializer
+        return TaskSerializer
+
+    def get_serializer_context(self):
+        return {"user_pk": self.request.user.id}
+
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.select_related("user").all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return ProfileCreateSerializer
+        return ProfileSerializer
+
+    def get_serializer_context(self):
+        return {"user": self.request.user}
