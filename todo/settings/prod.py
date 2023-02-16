4aee0e00551d235053de7b05709b78ed2dfd16a5
Saeid Heidari <saeiiid.heidari@gmail.com>
2023-02-16 13:41:31 +0330

    add database configs to prod settings.

--- todo/settings/prod.py
+++ todo/settings/prod.py
@@ -6,3 +6,14 @@ DEBUG = False
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
