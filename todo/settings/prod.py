aa67091d859c5adee7f3c63fd022cc4c704a5d87
Saeid Heidari <saeiiid.heidari@gmail.com>
2023-05-05 16:33:08 +0330

    update db settings for postgresql

--- todo/settings/prod.py
+++ todo/settings/prod.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
